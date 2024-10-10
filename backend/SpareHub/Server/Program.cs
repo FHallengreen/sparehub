920b0d37a24c457872f6ded31aded62b3227fb69
frederikhansen <frederikhallengreen@hotmail.com>
2024-10-10 14:01:28 +0200

    Changed cors

--- backend/SpareHub/Server/Program.cs
+++ backend/SpareHub/Server/Program.cs
@@ -1,45 +1,60 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.AzureAppServices;
 using Persistence;
 using Service;
-using Shared;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<AzureFileLoggerOptions>(builder.Configuration.GetSection("AzureLogging"));
+
+// Configure logging providers
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();    
+builder.Logging.AddDebug();    
+builder.Logging.AddAzureWebAppDiagnostics(); 
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Configuration.AddUserSecrets<Program>();
 
+// Add memory caching
+builder.Services.AddMemoryCache();  // Add this line
+
+// Add OrderService
 builder.Services.AddScoped<IOrderService, OrderService>();
 
-builder.Services.AddMemoryCache();
-
-var connectionString = string.Format("server={0};port={1};database={2};user={3};password={4}",
+// Configure the database connection string with SSL enabled
+var connectionString = string.Format("server={0};port={1};database={2};user={3};password={4};SslMode=Required",
     builder.Configuration.GetValue<string>("MYSQL_HOST"),
     builder.Configuration.GetValue<string>("MYSQL_PORT"),
     builder.Configuration.GetValue<string>("MYSQL_DATABASE"),
     builder.Configuration.GetValue<string>("MYSQL_USER"),
     builder.Configuration.GetValue<string>("MYSQL_PASSWORD"));
 
-
+// Register the DbContext with MySQL configuration
 builder.Services.AddDbContext<SpareHubDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 var app = builder.Build();
 
+// Configure CORS policy
 app.UseCors(corsPolicyBuilder =>
-    corsPolicyBuilder.WithOrigins("http://localhost:5173")
+    corsPolicyBuilder.WithOrigins(
+            "http://localhost:5173", 
+            "https://sparehub.fhallengreen.com"
+        )
         .AllowAnyMethod()
         .AllowAnyHeader());
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-if (!app.Environment.IsDevelopment() && Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") != "true")
+if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
-
 
 app.UseAuthorization();
 
