cfaebb2895784f2d7740c5bed482a31efc811c7f
Nikol <NikolajHelander@outlook.com>
2024-12-12 12:45:34 +0100

    portname?

--- backend/SpareHub/Shared/DTOs/VesselAtPort/VesselAtPortResponse.cs
+++ backend/SpareHub/Shared/DTOs/VesselAtPort/VesselAtPortResponse.cs
@@ -6,6 +6,8 @@ public class VesselAtPortResponse
 {
     public required string PortId { get; set; } = null!;
 
+    //public string PortName { get; set; } = null!;
+
     public List<VesselResponse> Vessels { get; set; } = [];
     
     public DateTime ArrivalDate { get; set; }
