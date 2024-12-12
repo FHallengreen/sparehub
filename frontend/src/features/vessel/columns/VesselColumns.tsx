eb25b611637e9091271e3c244e9cb85fe41385a8
Nikol <NikolajHelander@outlook.com>
2024-12-12 19:05:44 +0100

    updating columns and removing wrong filetype

--- frontend/src/features/vessel/columns/VesselColumns.tsx
+++ frontend/src/features/vessel/columns/VesselColumns.tsx
@@ -1,8 +1,6 @@
 import { GridColDef } from '@mui/x-data-grid';
 
 export const vesselColumns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', flex: 0.1, headerAlign: 'center', align: 'center' },
-    { field: 'owner_id', headerName: 'Owner ID', flex: 0.5, headerAlign: 'center', align: 'center' },
     { field: 'ownerName', headerName: 'Owner Name', flex: 0.5, headerAlign: 'center', align: 'center' },
     { field: 'name', headerName: 'Vessel Name', flex: 0.5, headerAlign: 'center', align: 'center' },
     { field: 'imoNumber', headerName: 'IMO Number', flex: 0.5, headerAlign: 'center', align: 'center' },
