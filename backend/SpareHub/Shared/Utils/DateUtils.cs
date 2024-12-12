ba576d83fa4181d069f1a0092c89675bbea14476
frede <frederikhallengreen@hotmail.com>
2024-12-12 13:49:59 +0100

    Changes to vesselatport response

---
+++ backend/SpareHub/Shared/Utils/DateUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Service.Utils;
+
+public class DateUtils
+{
+    public static string FormatToLocalDate(string? dateTimeString)
+    {
+        var dateTimeUtc = DateTime.Parse(dateTimeString ?? throw new ValidationException("Unable to format date"), null,
+            DateTimeStyles.AdjustToUniversal);
+
+        var localDateTime = dateTimeUtc.ToLocalTime();
+
+        return localDateTime.ToString("dd-MM-yyyy", CultureInfo.InvariantCulture);
+    }
+}
